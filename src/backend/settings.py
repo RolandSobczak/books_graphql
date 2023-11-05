787bd319663019906c53a4fef7357b8c04f90386
Roland Sobczak <rolandsobczak@icloud.com>
2023-11-05 15:43:33 +0100

    fix: missing type declarations in `Settings`

--- src/backend/settings.py
+++ src/backend/settings.py
@@ -8,17 +8,10 @@ env = Env()
 
 
 class Settings(BaseSettings):
-    LOG_LEVEL = env.log_level("LOG_LEVEL", logging.INFO)
-    MAX_PER_PAGE = env.int("MAX_PER_PAGE", 20)
+    LOG_LEVEL: int = env.log_level("LOG_LEVEL", logging.INFO)
+    MAX_PER_PAGE: int = env.int("MAX_PER_PAGE", 20)
 
-    CORS_CONFIG = {
-        "ALLOWED_ORIGINS": env.list("ALLOWED_ORIGINS"),
-        "ALLOW_CREDENTIALS": env.bool("ALLOW_CREDENTIALS"),
-        "ALLOWED_METHODS": env.list("ALLOWED_METHODS"),
-        "ALLOWED_HEADERS": env.list("ALLOWED_HEADERS"),
-    }
-
-    POSTGRES_CONFIG = {
+    POSTGRES_CONFIG: dict = {
         "USER": env("POSTGRES_USER"),
         "PASSWORD": env("POSTGRES_PASSWORD"),
         "HOST": env("POSTGRES_HOST"),
