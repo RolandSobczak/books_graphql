77471aab1e073fe9f28668262d6aca4822a91a79
Roland Sobczak <rolandsobczak@icloud.com>
2023-11-05 16:24:16 +0100

    fix: SessionManager fixed

--- src/backend/db.py
+++ src/backend/db.py
@@ -1,10 +1,24 @@
 from __future__ import annotations
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker, Session
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 from backend.settings import Settings
 
+settings = Settings()
+
+
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql+psycopg2://{settings.POSTGRES_CONFIG['USER']}:{settings.POSTGRES_CONFIG['PASSWORD']}"
+    f"@/{settings.POSTGRES_CONFIG['DB']}?host={settings.POSTGRES_CONFIG['HOST']}&port={settings.POSTGRES_CONFIG['PORT']}"
+)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
 
 Base = declarative_base()
 
@@ -14,38 +28,21 @@ class SessionManager:
     If session is already opened returns current session if not returns the newone.
     """
 
-    _attributes: dict = {}
-    _settings: Settings = Settings()
-    _engine = None
+    _attributes = {}
     _session = None
 
     def __init__(self):
         self.__dict__ = self._attributes
 
         if self._session is None:
-            self._open_new_session()
+            self.open_new_session()
 
     def __del__(self):
         self._session.close()
 
-    def _open_new_connection(self):
-        sqlalchemy_db_url = (
-            f"postgresql+psycopg2://{self._settings.POSTGRES_CONFIG['USER']}:{self._settings.POSTGRES_CONFIG['PASSWORD']}"
-            f"@/{self._settings.POSTGRES_CONFIG['DB']}?host={self._settings.POSTGRES_CONFIG['HOST']}&port={self._settings.POSTGRES_CONFIG['PORT']}"
-        )
-        return create_engine(sqlalchemy_db_url)
-
-    def _open_new_session(self):
-        if self._engine is None:
-            self._open_new_connection()
-
-        session_local = sessionmaker(
-            autocommit=False,
-            autoflush=False,
-            bind=self._engine,
-        )
-        self._session = session_local()
+    def open_new_session(self):
+        self._session = SessionLocal()
 
     @property
-    def session(self) -> Session:
+    def session(self):
         return self._session
