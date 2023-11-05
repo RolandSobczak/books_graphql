fa7790b3bd885774fc4faec06c5a96709293a0c2
Roland Sobczak <rolandsobczak@icloud.com>
2023-11-05 15:42:37 +0100

    feat: books and authors models

---
+++ src/backend/models/authors.py
@@ -0,0 +1,16 @@
+from typing import List
+
+from sqlalchemy import String, ForeignKey, Column, Table
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from backend.db import Base
+from backend.models.mixins import TimestampedMixin
+
+
+class AuthorModel(Base, TimestampedMixin):
+    __tablename__ = "authors"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    first_name: Mapped[str] = mapped_column(String(255))
+    last_name: Mapped[str] = mapped_column(String(255))
+    books: Mapped[List["backend.models.BookModel"]] = relationship(back_populates="author")
---
+++ src/backend/models/books.py
@@ -0,0 +1,18 @@
+from typing import List
+
+from sqlalchemy import String, ForeignKey, Column, Table
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from backend.db import Base
+from backend.models.mixins import TimestampedMixin
+
+
+class BookModel(Base, TimestampedMixin):
+    __tablename__ = "books"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(255))
+    description: Mapped[str] = mapped_column(String(255))
+    author_id: Mapped[int] = mapped_column(ForeignKey("authors.id"))
+    author: Mapped["backend.models.AuthorModel"] = relationship(back_populates="books")
+
